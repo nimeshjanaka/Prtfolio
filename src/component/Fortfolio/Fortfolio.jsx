b07d1a25c67431c71fe79c27554e65a4e46fc551
nimeshjanaka <nimeshjanaka.1995@gmail.com>
2024-06-01 21:38:56 +0530

    bug fixed

--- src/component/Fortfolio/Fortfolio.jsx
+++ src/component/Fortfolio/Fortfolio.jsx
@@ -7,7 +7,7 @@ const Fortfolio = () => {
     <div style={{ padding: "20px" }}>
       <h1>Latest Project</h1>
       <Row gutter={[20, 12]} justify="center">
-        <Col xs={24} sm={18} md={12} lg={10} xl={8}>
+        <Col xs={24} sm={24} md={12} lg={6}>
           <CardComponent
             title={"Web Development"}
             description={`when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
@@ -15,7 +15,7 @@ const Fortfolio = () => {
                         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with `}
           />
         </Col>
-        <Col xs={24} sm={18} md={12} lg={10} xl={8}>
+        <Col xs={24} sm={24} md={12} lg={6}>
           <CardComponent
             title={"Graphic Design"}
             description={`when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
@@ -23,7 +23,7 @@ const Fortfolio = () => {
                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with `}
           />
         </Col>
-        <Col xs={24} sm={18} md={12} lg={10} xl={8}>
+        <Col xs={24} sm={24} md={12} lg={6}>
           <CardComponent
             title={"Digital Marketing"}
             description={`when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
@@ -42,7 +42,7 @@ const Fortfolio = () => {
                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with `}
           />
         </Col>
-        <Col xs={24} sm={18} md={12} lg={10} xl={8}>
+        <Col xs={24} sm={24} md={12} lg={6}>
           <CardComponent
             title={"Graphic Design"}
             description={`when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
@@ -50,7 +50,7 @@ const Fortfolio = () => {
                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with `}
           />
         </Col>
-        <Col xs={24} sm={18} md={12} lg={10} xl={8}>
+        <Col xs={24} sm={24} md={12} lg={6}>
           <CardComponent
             title={"Digital Marketing"}
             description={`when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
